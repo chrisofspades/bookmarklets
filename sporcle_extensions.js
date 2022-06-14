1dd7e504c31c801908832dcf19fe47b5d4d328fb
chrisofspades <chrisofspades@gmail.com>
2022-06-14 15:57:07 -0700

    Create sporcle_extensions.js

---
+++ sporcle_extensions.js
@@ -0,0 +1,39 @@
+window.sporlceExtensions = {
+	lastNames: [
+		"johnson", 
+		"jones", 
+		"smith", 
+		"williams", 
+		"jackson", 
+		"brown", 
+		"green", 
+		"white", 
+		"davis", 
+		"anderson", 
+		"young", 
+		"lee", 
+		"james", 
+		"miller",
+		"thomas",
+		"thompson",
+		"harris",
+		"lewis",
+		"walker",
+		"robinson",
+		"robertson",
+		"richardson",
+		"jefferson",
+		"morris"
+	],
+	guessLastNames: () => {
+		var input = document.getElementById("gameinput");
+		var event = new Event('input', {
+			bubbles: true,
+			cancelable: true,
+		});
+		this.lastNames.forEach(name => {
+			input.value = name;
+			input.dispatchEvent(event);
+		});
+	}
+}
