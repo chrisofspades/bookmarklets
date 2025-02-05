b7a089c9d89b9a449ed184b50436f2fbedfe0cec
chrisofspades <chrisofspades@gmail.com>
2025-02-05 12:21:34 -0800

    Add clickFavorited() method to listchallenges_extensions.js

--- listchallenges_extensions.js
+++ listchallenges_extensions.js
@@ -3,5 +3,9 @@ window.listChallengesExtensions = {
 		jQuery("[data-item-id]").each((index, item) => jQuery(item).append(jQuery("<a target=_blank>")
 		   .text(jQuery(item).data("item-id"))
 		   .attr("href", `https://www.listchallenges.com/lists/containing-item/${jQuery(item).data("item-id")}`)));
+	},
+	clickFavorited() {
+		document.querySelectorAll(".item-favorite[data-item-id]")
+			.forEach(el => el.querySelector(".item-click-area").click());
 	}
 }
